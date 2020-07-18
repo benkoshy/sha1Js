53b9d21758e90f422d8789e5b548474d17c4b9f2
Joble Jose <joble.rays@gmail.com>
2020-07-19 04:38:17 +0530

    render hello world

---
+++ sha1.js
@@ -0,0 +1,23 @@
+// SHA1 Module
+function Module() {
+  this.rootSelector;
+
+  this.render = function() {
+    var textnode = document.createTextNode("Hello world: Joble's SHA app");
+    this.rootSelector.appendChild(textnode);
+  }
+}
+
+Module.prototype.init = function({node}) {
+  if (!node) {
+    console.warn("Provide a valid DOM object as node in init()");
+    return;
+  }
+  this.rootSelector = node;
+  this.render();
+  return {
+    name: this.name
+  }
+};
+
+var Sha1Module = new Module();
