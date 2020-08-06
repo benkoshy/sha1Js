263d03e9ae50167240798791ae90d7c04e707a3a
Joble Jose <joble.rays@gmail.com>
2020-08-07 04:03:14 +0530

    Support multiple instances

--- sha1.js
+++ sha1.js
@@ -1,62 +1,11 @@
 // SHA1 Module
-function Module() {
-
-  const selectElem =  function(id) {
-    return document.getElementById(id);
+class sha1Calc {
+  constructor(node, callback) {
+    this.rootSelector = node;
+    this.portCallback = callback;
   }
 
-  const setHtml = function(htmlString) {
-    var div = document.createElement('div');
-    div.innerHTML = htmlString.trim();
-    return div.firstChild; 
-  }
-
-  const Output = function(msgHtml) {
-    var outputDiv = selectElem("fileInfo");
-    outputDiv.innerHTML = msgHtml + outputDiv.innerHTML;
-    
-  }
-
-  const FileDragHover = function(e) {
-    e.stopPropagation();
-    e.preventDefault();
-    e.target.className = (e.type == "dragover" ? "hover" : "");
-  }
-
-  const FileSelectHandler = function(e) {
-  
-    // cancel event and hover styling
-    FileDragHover(e);
-
-    // fetch FileList object
-    var files = e.target.files || e.dataTransfer.files;
-    
-    // process all File objects
-    for (var i = 0, f; f = files[i]; i++) {
-      ParseFile(f);
-    }
-  }
-
-  const ParseFile = (file) => {
-    var data = {
-      'filename': file.name,
-      'type': file.type || '',
-      'size': file.size
-    }
-    // Callback parent with file data
-    if (typeof this.portCallback === "function") {
-      this.portCallback(JSON.stringify(data));
-    }
-
-    var outputHtml = `
-      <p>File name: <strong>${file.name}</strong> 
-      type: <strong> ${file.type} </strong> 
-      size: <strong> ${file.size} </strong> bytes</p>
-      `;
-    Output(outputHtml);
-  }
-
-  var fileInputMarkup = `
+  fileInputMarkup = `
     <div>
     <form id="SHAUploadForm" action="#" method="POST" enctype="multipart/form-data">      
       <input type="hidden" id="MAX_FILE_SIZE" name="MAX_FILE_SIZE" value="300000" />
@@ -74,46 +23,95 @@ function Module() {
     <div id="fileInfo">
     </div>
     </div>
-    `;
+  `;
 
-  this.InitDragDrop = function() {
-  
-    var fileselect = selectElem("SHAfileInput"),
-      filedrag = selectElem("SHAfileDrag"),
-      submitbutton = selectElem("SHAsubmitButton");
-
-    // file select
-    fileselect.addEventListener("change", FileSelectHandler, false);
-
-    // file drop
-    filedrag.addEventListener("dragover", FileDragHover, false);
-    filedrag.addEventListener("dragleave", FileDragHover, false);
-    filedrag.addEventListener("drop", FileSelectHandler, false);
-    filedrag.style.display = "block";
-
-    // remove submit button
-    // I have inlined the submit button so it does not display
-
-
+  SelectElem = function(id) {
+    return this.rootSelector.querySelector('#'+id)
   }
 
-  this.render = function() {
+  Output(msgHtml) {
+    var outputDiv = this.SelectElem("fileInfo");
+    outputDiv.innerHTML = msgHtml + outputDiv.innerHTML;
+    
+  }
 
+  SetHtml(markup) {
+    var div = document.createElement('div');
+    div.innerHTML = markup.trim();
+    return div.firstChild; 
+  }
+
+  FileDragHover(e) {
+      e.stopPropagation();
+      e.preventDefault();
+      e.target.className = (e.type == "dragover" ? "hover" : "");
+  }
+
+  FileSelectHandler(e) {
+
+    // cancel event and hover styling
+    this.FileDragHover(e);
+
+    // fetch FileList object
+    var files = e.target.files || e.dataTransfer.files;
+    
+    // process all File objects
+    for (var i = 0, f; f = files[i]; i++) {
+      this.ParseFile(f);
+    }
+  }
+
+  ParseFile(file) {
+    var data = {
+      'filename': file.name,
+      'type': file.type || '',
+      'size': file.size
+    }
+    // Callback parent with file data
+    if (typeof this.portCallback === "function") {
+      this.portCallback(JSON.stringify(data));
+    }
+
+    var outputHtml = `
+      <p>File name: <strong>${file.name}</strong> 
+      type: <strong> ${file.type} </strong> 
+      size: <strong> ${file.size} </strong> bytes</p>
+      `;
+    this.Output(outputHtml);
+  }
+
+  InitDragDrop() {
+  
+    var fileselect = this.SelectElem("SHAfileInput"),
+      filedrag = this.SelectElem("SHAfileDrag"),
+      submitbutton = this.SelectElem("SHAsubmitButton");
+
+    // file select
+    fileselect.addEventListener("change", this.FileSelectHandler.bind(this), false);
+
+    // file drop
+    filedrag.addEventListener("dragover", this.FileDragHover.bind(this), false);
+    filedrag.addEventListener("dragleave", this.FileDragHover.bind(this), false);
+    filedrag.addEventListener("drop", this.FileSelectHandler.bind(this), false);
+    filedrag.style.display = "block";
+  }
+
+  render() {
     // Link Stylesheet
     
     // please provide a separate stylesheet
     // because rails is failing - it cannot find the sytlesheet requested.
-/*
-    var link = document.createElement('link');  
-    link.rel = 'stylesheet';  
-    link.type = 'text/css'; 
-    link.href = 'style.css';  
-    document.getElementsByTagName('HEAD')[0].appendChild(link);
-*/
+
+    // var link = document.createElement('link');  
+    // link.rel = 'stylesheet';  
+    // link.type = 'text/css'; 
+    // link.href = 'style.css';  
+    // document.getElementsByTagName('HEAD')[0].appendChild(link);
+
 
     // Render file input html and scripts
     // var textnode = document.createTextNode("Hello world: Joble's SHA app");
-    var template = setHtml(fileInputMarkup);
+    var template = this.SetHtml(this.fileInputMarkup);
     this.rootSelector.appendChild(template);
     if (window.File && window.FileList && window.FileReader) {
       this.InitDragDrop();
@@ -124,19 +122,12 @@ function Module() {
   }
 }
 
-Module.prototype.init = function({node, callback}) {
-  if (!node) {
-    console.warn("Warning: Provide a valid DOM object as node in init()");
-    return;
+function Module() {
+  this.init = function({node, callback}) {
+    var sha1CalcObj = new sha1Calc(node, callback);
+    sha1CalcObj.render();
   }
-  this.rootSelector = node;
-  this.portCallback = callback;
-  this.render();
-  
-  return {
-    obj: this
-  }
-};
+}
 
 var Sha1Module = new Module();
 
